9677e98cfc142a73af81b0a78ea63baf0f8be0c1
sam505 <mainasamuel505@gmail.com>
2021-12-25 22:24:37 +0300

    Make changes

---
+++ Firebase/data_analysis.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import firebase_admin
+from firebase_admin import db
+
+cred_obj = firebase_admin.credentials.Certificate("Firebase/certificate.json")
+default_app = firebase_admin.initialize_app(cred_obj,
+                                            {
+                                                'databaseURL': "https://air-pollution-monitoring-a88eb-default-rtdb"
+                                                               ".firebaseio.com/"})
+ref = db.reference("/")
+
+
+def main():
+    read_data()
+
+
+def read_data():
+    datetime = []
+    mq7 = []
+    mq135 = []
+    temp = []
+    humidity = []
+    data = ref.get()
+    variables = data["data"].keys()
+    for variable in variables:
+        values = data["data"][variable]
+        values = values.split("#")
+        values = values[:4]
+        try:
+            datetime.append(variable)
+            mq7.append(float(values[0]))
+            mq135.append(float(values[1]))
+            temp.append(float(values[2]))
+            humidity.append(float(values[3]))
+        except ValueError:
+            print(values)
+            pass
+    print(len(datetime), len(mq7), len(mq135), len(temp), len(humidity))
+    values_dict = {
+        "timestamp": datetime,
+        "mq7": mq7,
+        "mq135": mq135,
+        "temperature": temp,
+        "humidity": humidity
+    }
+    df = pd.DataFrame(values_dict)
+    filename = "data.csv"
+    df.set_index("timestamp", inplace=True)
+    df.to_csv(filename)
+    return filename
+
+
+if __name__ == "__main__":
+    main()
---
+++ Firebase/visualization.py
@@ -0,0 +1,33 @@
+from data_analysis import read_data
+import streamlit as st
+import pandas as pd
+
+
+def main():
+    st.set_page_config(page_title="Air Pollution Dashboard", page_icon=None, layout='centered',
+                       initial_sidebar_state='auto')
+    st.title("Air Pollution Dashboard")
+    filename = read_data()
+    df = pd.read_csv(filename)
+    df.dropna()
+    df["timestamp"] = pd.to_datetime(df['timestamp'], unit='s')
+    df["timestamp"] = df["timestamp"].dt.tz_localize('UTC')
+    df.timestamp = df.timestamp.dt.tz_convert('Africa/Nairobi')
+    df = df.rename(columns={'timestamp': 'index'})
+    df_mq7 = df[["index", "mq7"]].set_index('index')
+    df_mq135 = df[["index", "mq135"]].set_index('index')
+    df_temp = df[["index", "temperature"]].set_index('index')
+    df_humidity = df[["index", "humidity"]].set_index('index')
+    st.text(df_mq7)
+    st.line_chart(df_mq7)
+    st.text(df_mq135)
+    st.line_chart(df_mq135)
+    st.text(df_temp)
+    st.line_chart(df_temp)
+    st.text(df_humidity)
+    st.line_chart(df_humidity)
+    st.text(df)
+
+
+if __name__ == "__main__":
+    main()
