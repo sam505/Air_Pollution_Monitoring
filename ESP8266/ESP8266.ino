ab60bb13ef45780baff04c9ee8f6c43dcd0f5139
sam505 <mainasamuel505@gmail.com>
2021-12-25 22:42:20 +0300

    Initial commit

---
+++ ESP8266/ESP8266.ino
@@ -0,0 +1,129 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <FirebaseArduino.h>
+
+// Set these to run example.
+#define FIREBASE_HOST "air-pollution-monitoring-a88eb-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "ufye7SB1GiULKeTgXZmwN7Tyy0pauHj6DzC2Uv2I"
+
+#define WIFI_SSID "GUEST"
+#define WIFI_PASSWORD "@m@in@123@"
+
+//#define WIFI_SSID "Galaxy"
+//#define WIFI_PASSWORD "hvsh9920"
+
+String data, epoch;
+int n = 0;
+
+unsigned int localPort = 2390;      // local port to listen for UDP packets
+
+/* Don't hardwire the IP address or we won't get the benefits of the pool.
+    Lookup the IP address for the host name instead */
+//IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char* ntpServerName = "time.nist.gov";
+
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
+void setup() {
+  Serial.begin(115200);
+
+  // connect to wifi.
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  udp.begin(localPort);
+}
+
+void loop() {
+
+  bool Sr = false;
+
+  while (Serial.available()) {
+
+    //get sensor data from serial put in sensor_data
+    data = Serial.readString();
+    Sr = true;
+  }
+
+  delay(1000);
+
+  //get a random server from the pool
+  WiFi.hostByName(ntpServerName, timeServerIP);
+
+  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(1000);
+
+  int cb = udp.parsePacket();
+  if (!cb) {
+  } else {
+    // We've received a packet, read the data from it
+    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+
+    if (Sr == true) {
+
+      if (data == "") {
+        Firebase.setString("data/" + String(epoch), "None");
+        Firebase.setInt("status/" + String(epoch), 0);
+      }
+
+      if (data != "") {
+        Firebase.setString("data/" + String(epoch), String(data));
+        delay(10);
+      }
+
+      if (Firebase.failed()) {
+        return;
+      }
+
+    }
+  }
+  Serial.flush(); // Delete all values in the serial monitor
+  delay(300000); // delay for 5 mins before checking for new values in the serial monitor
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress& address) {
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
+}
