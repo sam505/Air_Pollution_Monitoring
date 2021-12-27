82700c5af19181e6535a4cad34b645de7d665bf8
sam505 <mainasamuel505@gmail.com>
2021-12-27 18:25:32 +0300

    Add filtering code

--- Data_Analysis_Visualization/data_analysis.py
+++ Data_Analysis_Visualization/data_analysis.py
@@ -21,6 +21,7 @@ default_app = firebase_admin.initialize_app(cred_obj,
                                                 'databaseURL': "https://air-pollution-monitoring-a88eb-default-rtdb"
                                                                ".firebaseio.com/"})
 ref = db.reference("/")
+data = ref.get()
 os.remove(filename)
 
 
@@ -34,7 +35,6 @@ def read_data():
     mq135 = []
     temp = []
     humidity = []
-    data = ref.get()
     variables = data["data"].keys()
     for variable in variables:
         raw_values = data["data"][variable]
@@ -42,15 +42,15 @@ def read_data():
             assert raw_values[0] == "#"
             assert raw_values[1] != "#"
             values = raw_values.split("#")[1:5]
+            for i in range(4):
+                float(values[i])
+                assert values[i] != ""
         except AssertionError:
             values = raw_values.split("##")[1]
             values = values.split("#")
-        try:
-            for i in range(4):
-                float(values[i])
         except ValueError:
-            print(raw_values)
-            print(values)
+            values = raw_values.split("##")[1]
+            values = values.split("#")
             pass
         else:
             datetime.append(variable)
@@ -66,10 +66,10 @@ def read_data():
         "humidity": humidity
     }
     df = pd.DataFrame(values_dict)
-    filename = "Data_Analysis_Visualization/data.csv"
+    file_name = "Data_Analysis_Visualization/data.csv"
     df.set_index("timestamp", inplace=True)
-    df.to_csv(filename)
-    return filename
+    df.to_csv(file_name)
+    return file_name
 
 
 if __name__ == "__main__":
--- Data_Analysis_Visualization/visualization.py
+++ Data_Analysis_Visualization/visualization.py
@@ -5,17 +5,37 @@ import streamlit as st
 import pandas as pd
 
 
+def plot(df):
+    df_mq7 = df[["index", "mq7"]].set_index('index')
+    df_mq135 = df[["index", "mq135"]].set_index('index')
+    df_temp = df[["index", "temperature"]].set_index('index')
+    df_humidity = df[["index", "humidity"]].set_index('index')
+    st.subheader("MQ7 Sensor Data")
+    st.line_chart(df_mq7)
+    st.subheader("MQ135 Sensor Data")
+    st.line_chart(df_mq135)
+    st.subheader("Temperature Data")
+    st.line_chart(df_temp)
+    st.subheader("Humidity Data")
+    st.line_chart(df_humidity)
+
+
 def main():
-    st.set_page_config(page_title="Air Pollution Dashboard", page_icon="chart_with_upwards_trend", layout='wide',
-                       initial_sidebar_state='auto')
+    st.set_page_config(
+        page_title="Air Pollution Dashboard",
+        page_icon="chart_with_upwards_trend",
+        layout='wide',
+        initial_sidebar_state='auto'
+                       )
     st.title("Air Pollution Dashboard")
     filename = read_data()
     df = pd.read_csv(filename)
     df = df.dropna()
-    add_select_box = st.sidebar.selectbox(
+    select_data = st.sidebar.selectbox(
         "Which data would you like to visualize?",
-        ("Actual", "Predicted")
+        ("Actual", "Predicted", "Both")
     )
+    st.text(select_data)
     df["timestamp"] = pd.to_datetime(df['timestamp'], unit='s')
     df["timestamp"] = df["timestamp"].dt.tz_localize('UTC')
     df = df.rename(columns={'timestamp': 'index'})
@@ -28,23 +48,7 @@ def main():
         step=timedelta(days=1)
     )
     df = df[df.date.isin([date_range[0], date_range[1]])]
-    df_mq7 = df[["index", "mq7"]].set_index('index')
-    df_mq135 = df[["index", "mq135"]].set_index('index')
-    df_temp = df[["index", "temperature"]].set_index('index')
-    df_humidity = df[["index", "humidity"]].set_index('index')
-    # st.text(df_mq7)
-    st.subheader("MQ7 Sensor Data")
-    st.line_chart(df_mq7)
-    # st.text(df_mq135)
-    st.subheader("MQ135 Sensor Data")
-    st.line_chart(df_mq135)
-    # st.text(df_temp)
-    st.subheader("Temperature Data")
-    st.line_chart(df_temp)
-    # st.text(df_humidity)
-    st.subheader("Humidity Data")
-    st.line_chart(df_humidity)
-    # st.text(df)
+    plot(df)
 
 
 if __name__ == "__main__":
