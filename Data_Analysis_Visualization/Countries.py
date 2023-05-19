4faeb65a278a09fd0d4522a607c3335fb988333c
sam505 <mainasamuel505@gmail.com>
2023-05-20 02:04:58 +0300

    Update details

--- Data_Analysis_Visualization/Countries.py
+++ Data_Analysis_Visualization/Countries.py
@@ -5,7 +5,7 @@ from utilities import load_counties_data, plot_map
 
 
 st.set_page_config(
-        page_title="Air Pollution Dashboard",
+        page_title="Air Quality Dashboard",
         page_icon="chart_with_upwards_trend",
         layout='centered',
         initial_sidebar_state='auto',
@@ -22,7 +22,7 @@ st.write("Welcome to our air quality visualization dashboard! Here, you can expl
 
 st.write("We understand the importance of clean air and the impact it has on our health and environment. That's why we've created this dashboard to provide you with a comprehensive view of the air quality in these two countries, using data collected by IoT devices.")
 
-st.write("Our dashboard is updated every 15 minutes, providing you with the most current information on air quality levels in different cities across Kenya and Switzerland. You can use our dashboard to compare air quality levels between cities, track changes over time, and even see how air quality is affected by temperature and humidity.")
+st.write("Our dashboard is updated every 5 minutes, providing you with the most current information on air quality levels in different cities across Kenya and Switzerland. You can use our dashboard to compare air quality levels between cities, track changes over time, and even see how air quality is affected by temperature and humidity.")
 
 st.write("We hope that our air quality visualization dashboard will help you make informed decisions about your health and well-being, as well as contribute to a cleaner and healthier environment for everyone. Thank you for using our dashboard!")
 
