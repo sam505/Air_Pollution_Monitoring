5d7129557c4d22a7eeefe7ff7e951b110a267fb3
sam505 <mainasamuel505@gmail.com>
2023-05-19 23:55:51 +0300

    refactor code

--- Data_Analysis_Visualization/pages/1_🇰🇪_Kenya.py
+++ Data_Analysis_Visualization/pages/1_🇰🇪_Kenya.py
@@ -4,32 +4,46 @@ import plotly.graph_objects as go
 from utilities import *
 
 
+regions = {
+    "data": "Kericho",
+    "Home": "Nairobi"
+}
+
+
 def main():
     df = load_counties_data()
     country_name = __file__.split(".")[0].split("_")[-1]
     df = df[df.Country == country_name]
 
-    select_data = st.sidebar.selectbox(
-            "Select Region",
-            df.City.unique(),
-        )
-    
-    plot_map(df)
-    name = "data"
+    st.sidebar.subheader("Select Region")
+    region = [st.sidebar.button(name) for name in df.City.unique()]
+        
+    if region[0]:
+        name = "data"
+        plot_map(df[df.City == regions[name]])
+        
 
-    df = load_data(name)
+    elif region[1]:
+        name = "Home"
+        plot_map(df[df.City == regions[name]])
 
-    data_type = st.sidebar.radio(
-        "Choose the Data to Visualize",
-        ('Actual', 'Predicted'))
-    if data_type == "Actual":
-        select_data = st.sidebar.selectbox(
-            "Sample Data By:",
-            ("Actual", "Hourly", "Daily")
-        )
-        show_actual(df, select_data, name)
     else:
-        show_predictions(df, name)
+        plot_map(df)
+
+    if sum(region) != 0:
+        df = load_data(name)
+
+        data_type = st.sidebar.radio(
+            "Choose the Data to Visualize",
+            ('Actual', 'Predicted'))
+        if data_type == "Actual":
+            select_data = st.sidebar.selectbox(
+                "Sample Data By:",
+                ("Actual", "Hourly", "Daily")
+            )
+            show_actual(df, select_data, name)
+        else:
+            show_predictions(df, name)
 
 
 if __name__ == "__main__":
