bc1edcd2cc53b7af391a5c41922afc519a804e77
sam505 <mainasamuel505@gmail.com>
2023-05-20 20:42:24 +0300

    Refactor code

--- Data_Analysis_Visualization/pages/1_🇰🇪_Kenya.py
+++ Data_Analysis_Visualization/pages/1_🇰🇪_Kenya.py
@@ -8,9 +8,6 @@ regions = {
     "data": "Kericho",
     "Home": "Nairobi"
 }
-region = None
-data_type = None
-select_data = None
 
 
 @st.cache_data(experimental_allow_widgets=True)
@@ -36,24 +33,10 @@ def main():
     df = load_counties_data()
     country_name = __file__.split(".")[0].split("_")[-1]
     df = df[df.Country == country_name]
-
-    st.sidebar.subheader("Select Region")
-    region = [st.sidebar.button(name) for name in df.City.unique()]
-        
-    if region[0]:
-        name = list(regions.keys())[0]
-        plot_map(df[df.City == regions[name]])
-        initialize_sidebar(name)
-        
-
-    elif region[1]:
-        name = list(regions.keys())[1]
-        plot_map(df[df.City == regions[name]])
-        initialize_sidebar(name)
-
-    else:
-        plot_map(df)
-
+    region = st.sidebar.selectbox("Select Region", df.City.unique())
+    name = list(regions.keys())[list(regions.values()).index(region)]
+    plot_map(df[df.City == regions[name]])
+    initialize_sidebar(name)
         
 
 if __name__ == "__main__":
