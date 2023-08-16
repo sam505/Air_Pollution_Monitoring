100591379e45d2a643fb580de765c2b71f09b6e8
sam505 <mainasamuel505@gmail.com>
2023-08-16 20:58:06 +0300

    Add wifi setup

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,30 @@
-import machine
+from machine import Pin, ADC
 import time
+import network
 
-led = machine.Pin(2, machine.Pin.OUT)
+# TODO: Store these credentials as environment variables
+# ssid = "Galaxy A51 D22F"
+# password = "tqmy4554"
+
+ssid = "Home"
+password = "Home123M1"
+
+led = Pin(2, Pin.OUT)
+mq7_analog = ADC(Pin(34, Pin.IN))
+
+# connecting to Wi-Fi
+lan = network.WLAN(network.STA_IF)
+lan.active(True)
+
+if not lan.isconnected():
+    lan.connect(ssid, password)
+    if not lan.isconnected():
+        pass
+    print("Network Config:", lan.ifconfig())
 
 while True:
+    mq7_a = mq7_analog.read_u16()
+    print(mq7_a)
     print("LED toggling...")
     led.on()
     time.sleep(0.1)
